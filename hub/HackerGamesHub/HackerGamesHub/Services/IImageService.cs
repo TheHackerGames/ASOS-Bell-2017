222f83541dd8ed5f4398cc74af8c71b106c72e94
Adnan Gazi <adnan.gazi@asos.com>
2017-01-26 15:13:07 +0000

    create image service

---
+++ hub/HackerGamesHub/HackerGamesHub/Services/IImageService.cs
@@ -0,0 +1,8 @@
+﻿namespace HackerGamesHub.Services
+{
+    public interface IImageService
+    {
+        void SaveImage(string imageId, byte[] imageContent);
+        byte[] GetImage(string imageId);
+    }
+}
