caecf9797a15546e46314ed0f4022fe0418a9a14
asos-horaceb <horace.bellgam@asos.com>
2017-01-26 12:46:55 +0000

    add signal r client

---
+++ app/src/main/java/com/asos/hackergames/hackergamesdoorbell/DoorbellApp.java
@@ -0,0 +1,11 @@
+package com.asos.hackergames.hackergamesdoorbell;
+
+import android.app.Application;
+
+public class DoorbellApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
