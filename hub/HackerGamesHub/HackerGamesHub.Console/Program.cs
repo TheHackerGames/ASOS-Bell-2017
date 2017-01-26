78e7fb4fd9c5983a8778a9d5e3eef19e7f49c071
manuel.felicio <manuel.felicio@ASGLH-WL-10748>
2017-01-26 13:34:09 +0000

    Endpoint is now configured

--- hub/HackerGamesHub/HackerGamesHub.Console/Program.cs
+++ hub/HackerGamesHub/HackerGamesHub.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace HackerGamesHub.Console
             // use http://*:8080 to bind to all addresses. 
             // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
             // for more information.
-            string url = "http://localhost:8080";
+            string url = ConfigurationManager.AppSettings["Endpoint"];
             using (WebApp.Start<Startup>(url))
             {
                 System.Console.WriteLine("Server running on {0}", url);
